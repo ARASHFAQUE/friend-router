1d63126f34e259003cd181cfae3a5ab3d9e8eb6c
ARASHFAQUE <55973326+ARASHFAQUE@users.noreply.github.com>
2020-09-18 04:48:45 +0600

    first commit

---
+++ src/Components/FriendDetail/FriendDetail.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const FriendDetail = () => {
+
+    const { friendId } = useParams();
+    const [ friend, setFriend ] = useState({});
+    useEffect( () => {
+        const url = `https://jsonplaceholder.typicode.com/users/${friendId}`;
+        fetch(url)
+        .then( res => res.json())
+        .then( data => {
+            setFriend(data);
+            console.log(data);
+        })
+    }, []);
+
+    return (
+        <div>
+            <p>This is Detail : {friendId} </p>
+            <h3>Name : {friend.name}</h3>
+            <p>Email : {friend.email}</p>
+            <p>Phone : {friend.phone}</p>
+            <p>Website : {friend.website}</p>
+        </div>
+    );
+};
+
+export default FriendDetail;
